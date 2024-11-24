f5f85f7559e5b62cc104419605d31c87c6ea4cbe
Philip Titus <125900697+philiptitus@users.noreply.github.com>
2024-11-24 21:55:51 +0300

    NEW CALENDAR INTEGRATION AND NEW LLM

--- src/server/constants/API.tsx
+++ src/server/constants/API.tsx
@@ -1,6 +1,2 @@
 // config.ts
-<<<<<<< Updated upstream
 export const API_URL = 'https://vectorapi.pythonanywhere.com';
-=======
-export const API_URL = 'http://127.0.0.1:8000';
->>>>>>> Stashed changes
