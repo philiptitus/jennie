0b5f04026c86ae0aad0d62fb719134d25b3eed83
Philip <mrphilipowade@gmail.com>
2024-11-29 11:56:09 +0300

    restored services

--- src/views/admin/default/index.js
+++ src/views/admin/default/index.js
@@ -89,7 +89,7 @@ export default function UserReports() {
   return (
     <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='20px'>
-        <Popup/>
+        {/* <Popup/> */}
         {/* <Test/> */}
         <MiniStatistics
           startContent={
